318a764a789527de6bd36c9116a84072aa998b14
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2024-04-26 16:47:22 +0200

    img loader script and styles update

--- app/Views/public/pages/Welcome.php
+++ app/Views/public/pages/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 $lang = $_COOKIE['lang'] ?? null;
 ?>
-<div class="container-fluid vh-100">
+<div class="container-fluid vh-100 bg-light">
   <div class="row h-100">
     <div class="col-12 d-flex align-items-center justify-content-center flex-column h-100 mt-5">
       <button class="toast-btn btn btn-info p-3 px-5 text-white">Toast!</button>
