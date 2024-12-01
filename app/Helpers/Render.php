61752b4843cc9db074b7a089529e6a6e788403f6
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2024-12-01 15:48:22 +0100

    Render bugfix

--- app/Helpers/Render.php
+++ app/Helpers/Render.php
@@ -4,18 +4,27 @@ namespace App\Helpers;
 
 class Render
 {
-  public static function write($path, $params = [], $rewriteURL = false): string
+  public static function write($path, $params = []): string
   {
     ob_start();
 
     extract($params);
 
-    if (!$rewriteURL) {
-      require dirname(__DIR__, 2) . "/app/Views/" . $path;
-      return ob_get_clean();
-    } else {
-      require $rewriteURL;
-      return ob_get_clean();
+    $filePath = dirname(__DIR__, 2) . "/app/Views/" . $path;
+
+    if (!file_exists($filePath)) {
+      echo 'This file is doesnt exist!';
+      throw new \Exception("View file not found: " . $filePath);
     }
+
+    require $filePath;
+
+    $output = ob_get_clean();
+
+    if (!headers_sent()) {
+      header("Content-Type: text/html; charset=UTF-8");
+    }
+
+    return $output;
   }
 }
